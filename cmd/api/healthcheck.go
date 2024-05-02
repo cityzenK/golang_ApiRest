0647440746fe034d0a34d6efc7ee3985f615c34f
cityzenK <eliezergoh@gmail.com>
2024-05-02 12:29:41 -0600

    helper and updates

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,16 @@ import (
 )
 
 func (app *application) healthcheckHandle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: avaliable")
-	fmt.Fprintf(w, "enviroment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+
+	data := map[string]string{
+		"status":     "avaliable",
+		"enviroment": app.config.env,
+		"version":    version,
+	}
+	js := `{"status": "avaliable", "enviroment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-type", "application/json")
+
+	w.Write([]byte(js))
 }
